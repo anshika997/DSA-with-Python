9e5a5b76f2f2b8dc39851634349a519a3f6309d0
Anshika Singh <anshikasinghbisen2006@gmail.com>
2025-12-18 17:26:13 +0530

    Day 3: FizzBuzz with time and space complexity analysis

--- day 3/leetcode01_fizzbuzz.py
+++ day 3/leetcode01_fizzbuzz.py
@@ -1,25 +1,41 @@
-for num in range(1,101):
-    if(num % 3 == 0 and num % 5 == 0):
-        print("fizzbuzz",end =" ")
-    elif(num % 3 == 0):
-        print("fizz",end=" ")
-    elif(num % 5 == 0):
-        print("buzz",end=" ")
-    else:
-        print(num,end=" ")
-          
-        # leetcode solution  
-        
 # for num in range(1,101):
-num = int(input("Enter number : "))
-if(num % 3 == 0 and num % 5 == 0):
-        print("fizzbuzz")
-elif(num % 3 == 0):
-        print("fizz")
-elif(num % 5 == 0):
-        print("buzz")
-else:
-        print(num)
+#     if(num % 3 == 0 and num % 5 == 0):
+#         print("fizzbuzz",end =" ")
+#     elif(num % 3 == 0):
+#         print("fizz",end=" ")
+#     elif(num % 5 == 0):
+#         print("buzz",end=" ")
+#     else:
+#         print(num,end=" ")
+          
+#         # leetcode solution  
         
-        
-        
+# # for num in range(1,101):
+# num = int(input("Enter number : "))
+# if(num % 3 == 0 and num % 5 == 0):
+#         print("fizzbuzz")
+# elif(num % 3 == 0):
+#         print("fizz")
+# elif(num % 5 == 0):
+#         print("buzz")
+# else:
+#         print(num)
+
+from typing import List
+class Solution:
+    def FizzBuzz(self,n:int)->List[str]:          
+        ans=[]        
+        for i in range(1,n+1):
+            if i%3==0 and i %5==0:
+                ans.append("FizzBuzz")
+            elif i%3==0:
+                ans.append("Fizz")
+            elif i%5==0:
+                ans.append("Buzz")
+            else:   
+                ans.append(str(i))
+        return ans
+obj= Solution()
+n=int(input("Enter number : "))
+print(obj.FizzBuzz(n))
+            
