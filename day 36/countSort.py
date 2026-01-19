4ac8bce5adcb0ca07110470e7d2bfb4a366161ad
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-01-19 21:53:38 +0530

    Day 36 : countSort only for positive numbers

---
+++ day 36/countSort.py
@@ -0,0 +1,16 @@
+# only valid for non-negative integers
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        mx = max(nums)
+        freq=[0]*(mx+1)
+        for i in nums:
+            freq[i]+=1
+        nums=[]
+        for i in range(0,mx+1):
+            while freq[i]>0:
+                nums.append(i)
+                freq[i]-=1
+        return nums
+sol=Solution()
+print(sol.sortArray([5,2,3,1]))
