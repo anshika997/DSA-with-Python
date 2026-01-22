75e954472c9efca8c0799bb1a8c1f2fea2a49d20
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-01-22 21:37:58 +0530

    corrected code

---
+++ day 39/leetcode44.py
@@ -0,0 +1,13 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+        intervals.sort( key = lambda x:x[1])
+        n = len(intervals)
+        prev= 0 
+        count=1
+        for i in range (1,n):
+            if intervals[i][0]>=intervals[prev][1]:
+                count+=1
+                prev = i
+        return n-count
+sol = Solution()
+print(sol.eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]))
