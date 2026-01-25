1dab7c31b4383c7bc3a242e580c99bc6629dfbc3
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-01-25 18:42:52 +0530

    Day 42 : leetcode practice 46th solution

---
+++ day 42/leetcode46.py
@@ -0,0 +1,19 @@
+class Solution:
+        def lowerBound(self,nums,target):
+            n = len(nums)
+            l = 0 
+            r = n-1
+            ans = n
+            while l <= r:
+                mid =(l+r)//2
+                if nums[mid] >= target:
+                    ans = mid
+                    r = mid - 1
+                else :
+                    l = mid+1
+            return ans 
+        def searchInsert(self, nums: list[int], target: int) -> int:
+            return self.lowerBound(nums,target)
+sol = Solution()
+print(sol.searchInsert([1,3,5,6], 5))  # Output: 2
+
