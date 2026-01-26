f86bfb765ec38ff4d6c3b7ea768a32ed0129fc2f
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-01-26 13:49:24 +0530

    Day 43 : leetcode problem practice 49th

---
+++ day 43/leetcode49.py
@@ -0,0 +1,22 @@
+# it will give the nearest answer 
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x==0:
+            return 0
+        l =1 
+        r = x
+        ans = 1
+        while l <= r:
+            mid = (l+r)//2
+            midSquare= mid*mid
+            if midSquare > x :
+                r = mid-1
+            else:
+                
+                ans = mid
+                l  = mid+1
+        return ans 
+sol = Solution()
+print(sol.mySqrt(38))
+                
+    
