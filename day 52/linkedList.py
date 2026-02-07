b23c1e94ea36e961f3bf1358a5b072be34a5b9f9
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-02-07 13:35:17 +0530

    Day  55 : deleting the first ,last and kth element of the linkedlist

--- day 52/linkedList.py
+++ day 52/linkedList.py
@@ -22,13 +22,14 @@ def printLinkedList(head):
     while curr!=None:
         print(curr.data,end=" ")
         curr = curr.next
+print("\ntraverse linkedlist\n")
 printLinkedList(head)
 
 # insertion at the beginning of the node 
 newNode=Node(4)
 newNode.next=head
 head = newNode
-print("\t")
+print("\ninsertion at the beginning of the node\n")
 printLinkedList(head)
 
 # insertion at the end of the node 
@@ -38,7 +39,7 @@ curr = head
 while curr.next != None:
     curr = curr.next
 curr.next=newNode
-print()
+print("\ninsertion at the end of the node\n")
 printLinkedList(head)
 
 # insertion at the kth index of the node 
@@ -49,5 +50,81 @@ for i in range(k-1):
     curr = curr.next
 newNode.next= curr.next
 curr.next=newNode
-print()
+print("\ninsertion at the kth index of the node\n")
 printLinkedList(head)
+
+# deletion of the node at the beginning
+head = head.next
+print("\ndeleting the first node\n")
+
+printLinkedList(head)
+
+
+# deleting the last node 
+
+curr = head 
+while curr.next.next!=None:
+    curr = curr.next
+curr.next = None
+print("\ndeleting the last node\n ")
+printLinkedList(head)
+
+# deleting the kth node 
+
+k= 2
+curr = head 
+for i in range(k-1):
+    curr = curr.next
+curr.next = curr.next.next
+print("\ndeleting the kth node\n ")
+printLinkedList(head)
+print("\n")
+
+# how we make a Doubly linked list 
+class DoublyNode:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+
+a = Node(5)
+b = Node(3)
+c = Node(7)
+
+a.next=b
+b.prev=a
+b.next=c
+head = a 
+
+print(head.data)
+print(head.next.data)
+print(head.next.next.data)
+
+#circular linked list
+
+
+# how we make a circular linked list 
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+a = Node(5)
+b = Node(3)
+c = Node(7)
+
+a.next=b
+b.next=c
+c.next=a
+
+head = a 
+print(head.data)
+print(head.next.data)
+print(head.next.next.data)
+
+curr = head 
+while True:
+    print(curr.data,end=" ")
+    curr = curr.next
+    if curr==head:
+        break
