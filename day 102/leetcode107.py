0dc0db1dca854a6b0c06e408f34d71ed536b5b40
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-04-07 23:01:18 +0530

    Day 102 : leetcode practice problem

---
+++ day 102/leetcode107.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lemonadeChange(self, bills):
+        n = len(bills)
+        five = 0
+        ten = 0 
+        for i in range(0,n):
+            if bills[i]==5:
+                five+=1
+            elif  bills[i]==10:
+                if five>=1:
+                    five-=1
+                    ten+=1
+                else : 
+                    return False
+            else :
+                if five >=1 and ten >=1 :
+                    five -= 1 
+                    ten -= 1
+                elif five >=3:
+                    five -=3
+                else : 
+                    return False
+        return True 
+
+Solution = Solution()
+print(Solution.lemonadeChange([5,5,5,10,20]))  # Output: True
+print(Solution.lemonadeChange([5,5,10,10,20]))  # Output: False
