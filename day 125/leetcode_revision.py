a4387a538d6b323c9af01f90880416539ad5dce4
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-04-30 21:26:59 +0530

    Day 125 : revision time

---
+++ day 125/leetcode_revision.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums,val):
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)   # remove element
+            else:
+                i += 1
+        return len(nums)
+Solution = Solution()
+print(Solution.removeElement([3, 2, 2, 3], 3    ))
+print(Solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
