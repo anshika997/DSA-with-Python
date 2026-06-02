e2788340ee6c304955623a18cd6809f7d075c2e8
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-06-02 22:39:51 +0530

    Day 157 : leetcode 136

--- day 156/leetcode135.py
+++ day 156/leetcode135.py
@@ -1,10 +1,12 @@
 class Solution:
     def search(self, nums, target):
         n = len(nums)
+
         for i in range(n):
-            if target in nums :
-                return True 
+            if target in nums:
+                return True
             else:
                 return False
-print(Solution().search([-1,0,3,5,9,12], 9))
-print(Solution().search([-1,0,3,5,9,12], 2))
+Solution().search([1, 2, 3, 4, 5], 3)
+print(Solution().search([1, 2, 3, 4, 5], 3))
+print(Solution().search([1, 2, 3, 4, 5], 6))
---
+++ day 157/leetcode136.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums, target):
+        n = len(nums)
+        l = 0 
+        r = n-1
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid] == target :
+                return True 
+            if nums[l]==nums[mid]==nums[r]:
+                l +=1
+                r -=1
+            elif nums[l]<=nums[mid]:
+
+                if nums[l] <= target < nums[mid]:
+                    r = mid-1
+                else :
+                    l = mid+1
+            else:
+                if  nums[mid] < target <=nums[r]:
+                    l = mid+1
+                else :
+                    r = mid-1
+        return False 
+print(Solution().search([2,5,6,0,0,1,2], 0))
+print(Solution().search([2,5,6,0,0,1,2], 3))           
+            
+            
+        
