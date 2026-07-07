4deae3f64917b90ec6b7b5e64d8cd85d0a2fd045
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-07-07 23:05:16 +0530

    Day 192 : leetcode 171

---
+++ day 192/leetcode171.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generate(self, numRows):
+
+        triangle = []
+
+        for i in range(numRows):
+
+            row = [1]
+
+            if i > 0:
+
+                prev = triangle[i - 1]
+
+                for j in range(len(prev) - 1):
+
+                    row.append(prev[j] + prev[j + 1])
+
+                row.append(1)
+
+            triangle.append(row)
+
+        return triangle
+Solution = Solution()
+print(Solution.generate(5))
