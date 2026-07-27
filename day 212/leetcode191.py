e888d0bf839fac5a6ec1ec7319d9de83cc09eb93
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-07-27 13:40:37 +0530

    Day 212 : leetcode 191

---
+++ day 212/leetcode191.py
@@ -0,0 +1,33 @@
+class Solution:
+    def wordPattern(self, pattern, s) :
+
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for i in range(len(pattern)):
+
+            ch = pattern[i]
+            word = words[i]
+
+            if ch in char_to_word:
+                if char_to_word[ch] != word:
+                    return False
+            else:
+                char_to_word[ch] = word
+
+            if word in word_to_char:
+                if word_to_char[word] != ch:
+                    return False
+            else:
+                word_to_char[word] = ch
+
+        return True
+Solution = Solution()
+print(Solution.wordPattern("abba", "dog cat cat dog"))  # Output: True
+print(Solution.wordPattern("abba", "dog cat cat fish"))  # Output: False
+print(Solution.wordPattern("aaaa", "dog cat cat dog"))  # Output: False
