cb78140f3e9b475260117ad2daa4f2c524f69e4e
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-08-09 22:10:49 +0530

    Day 225 : leeetcode204

---
+++ day 225/leetcode204.py
@@ -0,0 +1,21 @@
+class Solution:
+    def toLowerCase(self, s):
+        word = s.lower()
+        return word
+Solution = Solution()
+print(Solution.toLowerCase('"Hello"'))
+
+
+# class Solution:
+#     def toLowerCase(self, s: str) -> str:
+#         return  s.lower()
+       
+
+# class Solution:
+#     def toLowerCase(self, s: str) -> str:
+#         result = ""
+#         for ch in s :
+#             result += ch.lower()
+#         return result 
+
+        
