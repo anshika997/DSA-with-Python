4bac59f3f4c5a8a7407db26d67a2200fce2c66f3
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-08-26 16:38:14 +0530

    Day 237 : leetcode 216

---
+++ day 237/leetcode216.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr):
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for i in range(1,len(arr)-1):
+            if arr[i+1] - arr[i] !=  diff:
+                return False
+        return True
+Solution=Solution()
+print(Solution.canMakeArithmeticProgression([3,5,1]))
+print(Solution.canMakeArithmeticProgression([1,2,4]))
