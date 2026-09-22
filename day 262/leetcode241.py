1a48e1564d16a585ebd4d90c4942c678640b3f75
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-09-23 01:16:33 +0530

    Day 262 : leetcode241

---
+++ day 262/leetcode241.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPairs(self, nums, k):
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            for j in range (i+1,n):
+                if nums[i] == nums[j] and (i * j)%k == 0 :
+                    count += 1
+        return count 
+Solution = Solution()
+print(Solution.countPairs([3,1,2,2,2,1,3],2))
